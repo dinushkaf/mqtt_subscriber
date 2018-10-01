f2e07c8fa2269e31dd57f7eb12a8169fcd9917dd
dinishkaf <dinimz@live.com>
2018-10-01 10:04:31 +0530

    update mongo driver and read me

--- src/mongo_driver.go
+++ src/mongo_driver.go
@@ -1,52 +1,40 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/mongodb/mongo-go-driver/mongo"
+	"gopkg.in/mgo.v2"
 )
 
-var mongoClient *mongo.Client
-var err error
-var database *mongo.Database
-var collection *mongo.Collection
+var collection *mgo.Collection
+var session mgo.Session
 
 func initMongo() bool {
-	//setting connection string
-	connectionString := fmt.Sprintf("mongodb://%s:%s@%s:%s", mongoConfig.username, mongoConfig.password,
-		mongoConfig.host, mongoConfig.port)
-	if mongoConfig.username == "" && mongoConfig.password == "" {
-		connectionString = fmt.Sprintf("mongodb://%s:%s",
-			mongoConfig.host, mongoConfig.port)
-	} else if mongoConfig.username == "" || mongoConfig.password == "" {
-		fmt.Println("Please provide MONGO_USER and MONGO_PASSWORD")
-		return false
-	}
-	fmt.Println("Connection String : " + connectionString)
-
 	//connecting with mongo db
-	mongoClient, err = mongo.Connect(context.Background(), connectionString)
+
+	connString := fmt.Sprintf("%s:%s", mongoConfig.host, mongoConfig.port)
+	println(connString)
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{connString},
+		Username: mongoConfig.username,
+		Password: mongoConfig.password,
+		Database: mongoConfig.database,
+	})
 	if err != nil {
-		fmt.Println("Mongo connection error occured!")
-		fmt.Printf("Connection String : %s\n", connectionString)
-		return false
+		fmt.Println(err)
 	}
 
-	//setting database and collection
-	database = mongoClient.Database(mongoConfig.database)
-	collection = database.Collection("sensor_data")
+	collection = session.DB(mongoConfig.database).C("sensor_data")
 
 	return true
 }
 
 //function for save in mongoDB
 func insertRecord(msg MQTTMessage) bool {
-	result, err := collection.InsertOne(context.Background(), msg)
+	err := collection.Insert(msg)
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
-	fmt.Printf("Inserted : %s", result.InsertedID)
 	return true
 }
